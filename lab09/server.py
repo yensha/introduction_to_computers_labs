1e79b3d5b6a4d70afe91a67532cd52a0fad595a9
yensha <113085268+yensha@users.noreply.github.com>
2022-11-29 22:00:40 +0800

    Create server.py

---
+++ lab09/server.py
@@ -0,0 +1,30 @@
+import socket
+
+#socket = host + port
+HOST = '10.3.141.1'
+PORT = 8000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+
+
+print('server start at: %s:%s' % (HOST, PORT))
+print('wait for connection...')
+
+while True:
+    conn, addr = s.accept()
+    print('connected by ' + str(addr))
+
+    while True:
+        indata = conn.recv(1024)
+        #先判斷有ctrl+c，有的話close connection
+        #不然繼續waiting for connecting...
+        if len(indata) == 0: 
+            conn.close()
+            print('client closed connection.')
+            break
+        print('recv: ' + indata.decode())
+
+        outdata = 'echo ' + indata.decode()
+        conn.send(outdata.encode())
